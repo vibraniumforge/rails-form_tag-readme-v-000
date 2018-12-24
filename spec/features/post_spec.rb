ddded76fc451f7f43c27d11baab96c880899d88e
Vibraniumforge <gregm9999@gmail.com>
2018-12-24 15:16:47 -0800

    Done.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe 'new post' do
   it 'ensures that the form route works with the /new action' do
@@ -11,6 +11,16 @@ describe 'new post' do
     expect(page).to have_content('Post Form')
   end
 
-
+  it "displays a new post form that redirects to the index page, which then contains the submitted post's title and description" do
+    visit new_post_path
+    fill_in 'post_title', with: 'My post title'
+    fill_in 'post_description', with: 'My post description'
+ 
+    click_on 'Submit Post'
+ 
+    expect(page.current_path).to eq(posts_path)
+    expect(page).to have_content('My post title')
+    expect(page).to have_content('My post description')
+  end
 
 end
